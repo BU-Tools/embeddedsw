4c88457faf74fa8c608c1179d66ad3af5f5e6eb6
Dan Gastler <dgastler@gmail.com>
2022-03-14 10:23:41 -0400

    Missing files

---
+++ lib/sw_apps/zynq_fsbl/src/si_data.c
@@ -0,0 +1,11 @@
+#include "si_data.h"
+
+
+void __atrribute__((weak)) siConfigInit(){
+  siConfigCount = 0;
+  siConfigs = NULL;
+}
+
+
+
+
---
+++ lib/sw_apps/zynq_fsbl/src/si_data.h
@@ -0,0 +1,18 @@
+#ifndef __SI_DATA__H_
+#define __SI_DATA__H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "xil_types.h"
+
+  void siConfigInit();
+  u32 siConfigCount;
+  sProgramSI * siConfigs;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
